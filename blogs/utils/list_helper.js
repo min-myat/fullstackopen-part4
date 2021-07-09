9ce10b03cb8a60929ffc2c764fd1342b805468d9
minmyatthu <minmyatthu2018.mlm@gmail.com>
2021-07-09 17:20:37 +0630

    exercise 4.4

--- blogs/utils/list_helper.js
+++ blogs/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let count = 0
+  blogs.map((blog) => (count += blog.likes))
+  return count
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
